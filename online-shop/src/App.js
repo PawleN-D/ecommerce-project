5e673c34f17a6d34a7d0105d99e706fb2a2a38c2
PawleN-D <sirpawle@gmail.com>
2021-01-31 03:01:26 +0200

    added a header component, assets folder and deleted the images folder

--- online-shop/src/App.js
+++ online-shop/src/App.js
@@ -5,10 +5,12 @@ import "./App.css";
 
 import Homepage from "./pages/homepage/homepage";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage} />
