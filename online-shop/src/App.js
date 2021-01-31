e2239e58f8845afc6cc509fbd6832e8790bda9bd
PawleN-D <sirpawle@gmail.com>
2021-01-31 05:04:13 +0200

    added styling to the button

--- online-shop/src/App.js
+++ online-shop/src/App.js
@@ -6,6 +6,7 @@ import "./App.css";
 import Homepage from "./pages/homepage/homepage";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
