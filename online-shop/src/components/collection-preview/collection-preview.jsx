047f83e8621d4f9608ad2cfc0e64148ad75a60ac
PawleN-D <sirpawle@gmail.com>
2021-02-08 01:15:47 +0200

    updated shop data, created cart utils to group items, styled the add to cart button

--- online-shop/src/components/collection-preview/collection-preview.jsx
+++ online-shop/src/components/collection-preview/collection-preview.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import CollectionItem from "../collection-item/collection-item";
 
-
 import "./collection-preview.styles.scss";
 
 const CollectionPreview = ({ title, items }) => (
@@ -10,9 +9,8 @@ const CollectionPreview = ({ title, items }) => (
     <div className="preview">
       {items
         .filter((item, idx) => idx < 4)
-        .map(({ id, ...otherItemProps }) => (
-
-          <CollectionItem key={id} {...otherItemProps} />
+        .map((item) => (
+          <CollectionItem key={item.id} item={item} />
         ))}
     </div>
   </div>
