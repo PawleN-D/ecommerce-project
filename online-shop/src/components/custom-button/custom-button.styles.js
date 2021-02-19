74535bcec4e223c33ef89dcf83b9c9e578a17c9a
PawleN-D <sirpawle@gmail.com>
2021-02-20 00:40:14 +0200

    pulled data from the firestore into shop page

---
+++ online-shop/src/components/custom-button/custom-button.styles.js
@@ -0,0 +1,81 @@
+import styled, { css } from "styled-components";
+
+const buttonStyles = css`
+  background-color: black;
+  color: white;
+  border: none;
+
+  &:hover {
+    background-color: white;
+    color: black;
+    border: 1px solid black;
+  }
+`;
+
+const customButtonStyles = css`
+  width: 80%;
+  opacity: 0.7;
+  position: absolute;
+  top: 255px;
+  display: none;
+
+  &:hover {
+    background-color: black;
+    color: white;
+    border: none;
+    opacity: 0.85;
+    display: flex;
+  }
+`;
+
+const invertedButtonStyles = css`
+  background-color: white;
+  color: black;
+  border: 1px solid black;
+
+  &:hover {
+    background-color: black;
+    color: white;
+    border: none;
+  }
+`;
+
+const googleSignInStyles = css`
+  background-color: #4285f4;
+  color: white;
+
+  &:hover {
+    background-color: #357ae8;
+    border: none;
+  }
+`;
+
+const getButtonStyles = (props) => {
+  if (props.isGoogleSignIn) {
+    return googleSignInStyles;
+  }
+
+  //   if (props.inverted && props.customButton) {
+  //     return customButtonStyles;
+  //   }
+
+  return props.inverted ? invertedButtonStyles : buttonStyles;
+};
+
+export const CustomButtonContainer = styled.button`
+  min-width: 165px;
+  width: auto;
+  height: 50px;
+  letter-spacing: 0.5px;
+  line-height: 50px;
+  padding: 5px 15px 5x 15px;
+  font-size: 15px;
+  text-transform: uppercase;
+  font-family: "Open Sans Condensed";
+  font-weight: lighter;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+
+  ${getButtonStyles}
+`;
--- online-shop/src/pages/homepage/homepage.jsx
+++ online-shop/src/pages/homepage/homepage.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
 import Directory from "../../components/directory/directory.component";
-import "./homepage.scss";
+
+import { HomePageContainer } from "./homepage.styles";
 
 const Homepage = () => {
   return (
-    <div className="homepage">
+    <HomePageContainer>
       <Directory />
-    </div>
+    </HomePageContainer>
   );
 };
 
