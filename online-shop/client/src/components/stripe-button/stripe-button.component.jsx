25e4dd13dda76c12c49542925cfbad3d619c0ed8
PawleN-D <sirpawle@gmail.com>
2021-03-06 02:12:57 +0200

    adjusted cra into client and server to help stripe payments

--- online-shop/client/src/components/stripe-button/stripe-button.component.jsx
+++ online-shop/client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
@@ -7,8 +8,23 @@ const StripeCheckoutButton = ({ price }) => {
     "pk_test_51IJqDrByBMdWqTQ5w5uz0zIdJ96bVXjy4tFbL2sJdlmPQfJyVpSC4vNJRemsFvdtbmPbv6U4Hrsv4HCz6mstntLn009wTf2EK9";
 
   const onToken = (token) => {
-    console.log(token);
-    alert("Payment Successful");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert("Payment Successful!");
+      })
+      .catch((error) => {
+        console.log("Payment error: ", JSON.parse(error));
+        alert(
+          "There was an issue with your payment. Please use the provided credit card!"
+        );
+      });
   };
 
   return (
